ee407250dd331a103d456d22c0607d6df838a868
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-31 12:00:25 -0500

    Resultados

--- ConsultarResultados.php
+++ ConsultarResultados.php
@@ -12,7 +12,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Servicio social - Administración</title>
+    <title>Resultados de las evaluaciones diagnósticas - Administración</title>
 </head>
 
 <body>
