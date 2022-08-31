1049e5a4e33f221bad53ea0d64b9fa610065065c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-08-31 11:59:20 -0500

    Resultados HSE

--- ConsultarResultados.php
+++ ConsultarResultados.php
@@ -108,6 +108,27 @@ session_start();
             }
             ?>
         </table>
+        <br>
+        <p class="Descripción-Artículo">Resultados Historia socioeconómica de México:</p>
+        <table class="InsertarResultados Examen">
+            <?php
+            $consulta = "SELECT * FROM `resultados_hse`";
+            $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+            echo ('<tr>');
+            echo ('<th>ID</th>');
+            echo ('<th>Nombre</th>');
+            echo ('<th>Calificación</th>');
+            echo ('</tr>');
+            while ($columna = mysqli_fetch_array($resultado)) {
+                echo ("<tr>");
+                echo ("<td>" . $columna['ID'] . "</td>");
+                echo ("<td>" . $columna['Alumno'] . "</td>");
+                echo ("<td>" . $columna['Calificación'] . "</td>");
+
+                echo ("</tr>");
+            }
+            ?>
+        </table>
 
         <footer>
             <div id="RedesSociales" align="center">
